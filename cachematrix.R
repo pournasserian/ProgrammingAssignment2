e3df9de40e81623577417827336ec22bdceba7a3
Amir Pournasserian <amir.pournasserian@live.com>
2014-08-13 22:10:58 +0430

    Commit the code

--- cachematrix.R
+++ cachematrix.R
@@ -3,40 +3,49 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(in_matrix = matrix()) {
-  
-  cached_out_matric <- NULL
-  
-  set <- function(in_matrix) {
-    cached_in_matrix <<- in_matrix
-    cached_out_matric <<- NULL
-  }
-  
-  get <- function() cached_in_matrix
-  
-  setinverse <- function(out_matrix) cached_out_matric <<- out_matrix
-  
-  getinverse <- function() cached_out_matric
-  
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
-  
+makeCacheMatrix <- function(x = matrix()) {
+    
+    inverse <- NULL
+    
+    set <- function(y){
+        x <<- y
+        inverse <<- NULL
+    }
+    
+    get <- function() x
+    setinverse <- function(inv) {
+        inverse <<- inv
+    }
+    getinverse <- function() inverse
+    
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
+        
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- inv(data, ...)
-  x$setinverse(m)
-  m
-  
+    ## Return a matrix that is the inverse of 'x'
+
+    m <- x$getinverse()
+    
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    data <- x$get()
+    
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+    
 }
+
+
+
+
